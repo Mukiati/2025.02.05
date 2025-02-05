50aa5cb0635194ffb51e94937f4747bf5ab0f7c9
Bertók Attila <bertok.attila.21i@egressy.info>
2025-02-05 11:32:12 +0100

    Add project files.

---
+++ 2025.02.05/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace _2025._02._05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string text2;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public string Text2
+        {
+            get { return text2; }
+            set 
+            { 
+                text2 = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
